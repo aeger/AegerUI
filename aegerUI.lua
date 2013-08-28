bc58d60061ac90ac75c477bd68c9eb50d9d107cf
aeger <almty1@gmail.com>
2013-08-27 21:23:49 -0700

    Cleaned up
    first working version

---
+++ aegerUI.lua
@@ -0,0 +1,75 @@
+local aegerUI = ...
+local MEDIAPATH = "Interface\\AddOns\\" .. aegerUI .. "\\Media\\"
+
+local BottomBarFrame1 = CreateFrame("Frame", "BottomBarFrame1", UIParent)
+BottomBarFrame1:Hide()
+
+local BottomBarFrame2 = CreateFrame("Frame", "BottomBarFrame2", UIParent)
+BottomBarFrame2:Hide()
+
+local DisplayBars = CreateFrame("Frame")
+DisplayBars:RegisterEvent("ADDON_LOADED")
+DisplayBars:RegisterEvent("ONUPDATE")
+DisplayBars:RegisterEvent("ONLOAD")
+
+local function BbarDisplay()
+if Bbars == 1 then
+  BottomBarFrame1:Show()
+  BottomBarFrame2:Hide()
+ else
+if Bbars == 2 then
+  BottomBarFrame2:Show()
+  BottomBarFrame1:Hide()
+  end
+end
+end
+
+DisplayBars:SetScript("OnEvent", function(self, event, ...)
+BbarDisplay()
+end)
+
+BottomBarFrame1:SetScript("OnShow", function(self)
+  self:SetScript("OnShow", nil)
+  PetBattleFrame:HookScript("OnShow",function() self:Hide() end)
+  PetBattleFrame:HookScript("OnHide",function() self:Show() end)
+    
+  local Bar1display = self:CreateTexture(nil, "BACKGROUND")
+    Bar1display:SetWidth(943)
+    Bar1display:SetHeight(43)
+    Bar1display:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, -3)
+	Bar1display:SetPoint("CENTER", UIParent, "CENTER")
+    Bar1display:SetTexture(MEDIAPATH .. "btrounded")
+	Bar1display:SetVertexColor(0, 0, 0, .5)
+	
+  local Bar1BorderDisplay = self:CreateTexture(nil, "BORDER")
+    Bar1BorderDisplay:SetWidth(947)
+	Bar1BorderDisplay:SetHeight(47)
+	Bar1BorderDisplay:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, -3)
+	Bar1BorderDisplay:SetPoint("CENTER", UIParent, "CENTER")
+	Bar1BorderDisplay:SetTexture(MEDIAPATH .. "btroundedborder")
+	local_,class = UnitClass("player")
+	Bar1BorderDisplay:SetVertexColor(RAID_CLASS_COLORS[class].r, RAID_CLASS_COLORS[class].g, RAID_CLASS_COLORS[class].b, 1.0)
+end)
+
+BottomBarFrame2:SetScript("OnShow", function(self)
+  self:SetScript("OnShow", nil)
+  PetBattleFrame:HookScript("OnShow",function() self:Hide() end)
+  PetBattleFrame:HookScript("OnHide",function() self:Show() end)
+  
+  local Bar2display = self:CreateTexture(nil, "BACKGROUND")
+    Bar2display:SetWidth(943)
+    Bar2display:SetHeight(84)
+    Bar2display:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, -4)
+	Bar2display:SetPoint("CENTER", UIParent, "CENTER")
+    Bar2display:SetTexture(MEDIAPATH .. "btrounded")
+	Bar2display:SetVertexColor(0, 0, 0, .5)
+	
+	local Bar2BorderDisplay = self:CreateTexture(nil, "BORDER")
+    Bar2BorderDisplay:SetWidth(947)
+	Bar2BorderDisplay:SetHeight(90)
+	Bar2BorderDisplay:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, -4)
+	Bar2BorderDisplay:SetPoint("CENTER", UIParent, "CENTER")
+	Bar2BorderDisplay:SetTexture(MEDIAPATH .. "btroundedborder")
+	local_,class = UnitClass("player")
+	Bar2BorderDisplay:SetVertexColor(RAID_CLASS_COLORS[class].r, RAID_CLASS_COLORS[class].g, RAID_CLASS_COLORS[class].b, 1.0)
+end)
--- setup.lua
+++ setup.lua
@@ -1,9 +1,175 @@
-local AegerUI = ...
-local MEDIAPATH = "Interface\\AddOns\\" .. AegerUI .. "\\Media\\"
+local aegerUI = ...
+local MEDIAPATH = "Interface\\AddOns\\" .. aegerUI .. "\\Media\\"
 
 local FONT = "Fonts\\FRIZQT__.ttf"
 
 local ADDONS = {
-	"Chinchilla",
-	"kgPanels",
-}
+  "Chinchilla",
+  "Bartender4",
+  }
+  
+local TEXT = {
+
+  Install = "Install",
+  Cancel = "Cancel",
+  
+  }
+ 
+ --Frames------------------------------------------------------------------
+  
+local SetupFrame = CreateFrame("Frame", "aegerUISetupFrame", UIParent)
+SetupFrame:Hide()
+
+local FirstRunFrame = CreateFrame("Frame")
+FirstRunFrame:RegisterEvent("PLAYER_LOGIN")
+
+--Events---------------------------------------------------------------------
+
+FirstRunFrame:SetScript("OnEvent", function(self, event, ...)
+self:RegisterEvent("CINEMATIC_START")
+self:RegisterEvent("CINEMATIC_STOP")
+if event == "CINEMATIC_START" then 
+  SetupFrame:Hide()
+elseif event == "CINEMATIC_STOP" then
+ if FirstRun == nil then
+  SetupFrame:Show()
+ end
+ else
+ if FirstRun == nil then
+  SetupFrame:Show()
+  end
+ end
+ end)
+
+--Functions------------------------------------------------------------------
+
+ local function SetProfiles()
+  if IsAddOnLoaded("Bartender4") then
+    Bartender4.db:SetProfile("aegerUI1bar")
+  end
+  if IsAddOnLoaded("Chinchilla") then
+    Chinchilla.db:SetProfile("aegerUI")
+  end
+end
+
+local function SetBbars1()
+  Bbars = 1
+end
+
+local function SetBbars2()
+  Bbars = 2
+end
+
+local function DoSetup()
+PlaySoundFile(MEDIAPATH .. "Sound\\click.mp3")
+SetupFrame:Hide()
+local reload
+local AceAddon = LibStub("AceAddon-3.0")
+FirstRun = 0
+SetProfiles()
+SetBbars1()
+reload = true
+  if reload then
+    ReloadUI()
+  end
+end
+  
+
+
+
+local function BigButton_OnEnter(self)
+	self:GetNormalTexture():SetVertexColor(0, 1, 0)
+end
+
+local function Button_OnLeave(self)
+	self:GetNormalTexture():SetVertexColor(1, 1, 1)
+end
+
+
+
+--Setup frame----------------------------------------------------------------
+
+SetupFrame:SetScript("OnShow", function(self)
+  self:SetScript("OnShow", nil)
+  self:SetPoint("CENTER")
+  self:SetSize(400, 400)
+  self:SetFrameStrata("TOOLTIP")
+  self:SetFrameLevel("18")
+
+  local Backdrop = self:CreateTexture(nil, "BACKGROUND")
+    Backdrop:SetAllPoints(UIParent)
+    Backdrop:SetTexture(0, 0, 0, 0.9)
+    self.Backdrop = Backdrop
+	
+--------------------
+--  INSTALL Button  --
+--------------------
+
+	local SetupButton = CreateFrame("Button", nil, self)
+	SetupButton:SetPoint("TOP", SetupFrame, "BOTTOM")
+	SetupButton:SetSize(346, 68)
+	SetupButton:SetNormalTexture(MEDIAPATH .. "BigButton")
+	SetupButton:SetScript("OnEnter", BigButton_OnEnter)
+	SetupButton:SetScript("OnLeave", Button_OnLeave)
+	SetupButton:SetScript("OnClick", DoSetup)
+	self.SetupButton = SetupButton
+
+	local SetupText = SetupButton:CreateFontString(nil, "OVERLAY")
+	SetupText:SetPoint("LEFT")
+	SetupText:SetPoint("RIGHT")
+	SetupText:SetFont(FONT, 32, "OUTLINE")
+	SetupText:SetAlpha(0.8)
+	SetupText:SetText(TEXT.Install)
+	SetupButton:SetFontString(SetupText)
+	
+--------------------
+--  CANCEL Button  --
+--------------------
+
+	local CancelButton = CreateFrame("Button", nil, self)
+	CancelButton:SetPoint("TOP", SetupButton, "BOTTOM", 0, 2)
+	CancelButton:SetSize(346, 68)
+	CancelButton:SetNormalTexture(MEDIAPATH .. "BigButton")
+	CancelButton:SetScript("OnEnter", BigButton_OnEnter)
+	CancelButton:SetScript("OnLeave", Button_OnLeave)
+	CancelButton:SetScript("OnClick", function(this)
+		PlaySoundFile(MEDIAPATH .. "Sound\\click.mp3")
+		FirstRun = nil
+		SetupFrame:Hide()
+		UIParent:SetAlpha(1)
+	end)
+	self.CancelButton = CancelButton
+	local CancelText = CancelButton:CreateFontString(nil, "OVERLAY")
+	CancelText:SetPoint("LEFT")
+	CancelText:SetPoint("RIGHT")
+	CancelText:SetFont(FONT, 32, "OUTLINE")
+	CancelText:SetAlpha(0.8)
+	CancelText:SetText(TEXT.Cancel)
+	CancelButton:SetFontString(CancelText)
+end)
+
+SlashCmdList.INSTALL = function()
+	FirstRun = nil
+	SetupFrame:Show()
+end
+SLASH_INSTALL1 = "/install"
+
+SlashCmdList.ONEBAR = function()
+	Bbars = 1
+	if IsAddOnLoaded("Bartender4") then
+    Bartender4.db:SetProfile("aegerUI1bar")
+  end
+	
+end
+SLASH_ONEBAR1 = "/bar1"
+
+SlashCmdList.TWOBAR = function()
+	Bbars = 2
+	if IsAddOnLoaded("Bartender4") then
+    Bartender4.db:SetProfile("aegerUI2bar")
+  end
+	
+end
+SLASH_TWOBAR1 = "/bar2"
+
+
