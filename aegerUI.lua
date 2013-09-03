02e35d578b92326727afc890517495ad10d5244f
aeger <almty1@gmail.com>
2013-09-02 23:44:59 -0700

    Added combat coloring
    Bottombar border turns red during combat

--- aegerUI.lua
+++ aegerUI.lua
@@ -8,6 +8,11 @@ BottomBarFrame1:Hide()
 local BottomBarFrame2 = CreateFrame("Frame", "BottomBarFrame2", UIParent)
 BottomBarFrame2:Hide()
 
+local Bar2BorderFrame = CreateFrame("Frame", "Bar2BorderFrame", UIParent)
+Bar2BorderFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
+Bar2BorderFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
+Bar2BorderFrame:Hide()
+
 local DisplayBars = CreateFrame("Frame")
 DisplayBars:RegisterEvent("ADDON_LOADED")
 DisplayBars:RegisterEvent("ONUPDATE")
@@ -22,20 +27,22 @@ if ( not MainMenuBar.busy and not UnitHasVehicleUI("player") ) then
   if Bbars == 1 then
     BottomBarFrame1:Show()
     BottomBarFrame2:Hide()
+	Bar2BorderFrame:Hide()
  end
   
 if Bbars == 2 then
   BottomBarFrame2:Show()
   BottomBarFrame1:Hide()
+  Bar2BorderFrame:Show()
 end
 
 elseif
   UnitHasVehicleUI("player") then
     BottomBarFrame1:Hide()
     BottomBarFrame2:Hide()
+	Bar2BorderFrame:Hide()
 end
 end
-
 
 DisplayBars:SetScript("OnEvent", function(self, event, ...)
 BbarDisplay()
@@ -68,19 +75,37 @@ BottomBarFrame2:SetScript("OnShow", function(self)
   PetBattleFrame:HookScript("OnShow",function() self:Hide() end)
   PetBattleFrame:HookScript("OnHide",function() self:Show() end)
   
-  local Bar2display = self:CreateTexture(nil, "BACKGROUND")
+    local Bar2display = self:CreateTexture(nil, "BACKGROUND")
     Bar2display:SetWidth(943)
     Bar2display:SetHeight(84)
     Bar2display:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, -4)
 	Bar2display:SetPoint("CENTER", UIParent, "CENTER")
     Bar2display:SetTexture(MEDIAPATH .. "btrounded")
 	Bar2display:SetVertexColor(0, 0, 0, .5)
-	
-	local Bar2BorderDisplay = self:CreateTexture(nil, "BORDER")
-    Bar2BorderDisplay:SetWidth(947)
-	Bar2BorderDisplay:SetHeight(90)
-	Bar2BorderDisplay:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, -4)
-	Bar2BorderDisplay:SetPoint("CENTER", UIParent, "CENTER")
-	Bar2BorderDisplay:SetTexture(MEDIAPATH .. "btroundedborder")
-	Bar2BorderDisplay:SetVertexColor(classcolor.r, classcolor.g, classcolor.b, 1.0)
+end)
+
+Bar2BorderFrame:SetScript("OnShow", function(self)
+self:SetScript("OnShow", nil)
+PetBattleFrame:HookScript("OnShow",function() self:Hide() end)
+PetBattleFrame:HookScript("OnHide",function() self:Show() end)
+
+local Bar2Bordertex = self:CreateTexture(nil, "BORDER")
+Bar2Bordertex:SetSize(947, 90)
+Bar2Bordertex:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, -4)
+Bar2Bordertex:SetPoint("CENTER", UIParent, "CENTER")
+Bar2Bordertex:SetTexture(MEDIAPATH .. "btroundedborder")
+Bar2Bordertex:SetVertexColor(classcolor.r, classcolor.g, classcolor.b, 1.0)
+end)
+
+Bar2BorderFrame:SetScript("OnEvent", function(self, event, ...)
+  local Bar2Bordertex = self:CreateTexture(nil, "BORDER")
+  Bar2Bordertex:SetSize(947, 90)
+  Bar2Bordertex:SetPoint("BOTTOM", UIParent, "BOTTOM", 0, -4)
+  Bar2Bordertex:SetPoint("CENTER", UIParent, "CENTER")
+  Bar2Bordertex:SetTexture(MEDIAPATH .. "btroundedborder")
+  if event == "PLAYER_REGEN_DISABLED" then
+  Bar2Bordertex:SetVertexColor(1, 0, 0)
+  elseif event == "PLAYER_REGEN_ENABLED" then
+  Bar2Bordertex:SetVertexColor(classcolor.r, classcolor.g, classcolor.b, 1.0)
+  end
 end)
