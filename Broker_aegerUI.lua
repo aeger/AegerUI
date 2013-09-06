fed5607d2ebbfb10a985a9f2c872229b48b815fb
aeger <almty1@gmail.com>
2013-09-05 19:01:34 -0700

    Fixed data broker toggle

--- Broker_aegerUI.lua
+++ Broker_aegerUI.lua
@@ -1,4 +1,5 @@
-local L_aegerUI = LibStub("AceLocale-3.0"):GetLocale("aegerUI")
+--local L_aegerUI = LibStub("AceLocale-3.0"):GetLocale("aegerUI")
+local MEDIAPATH = "Interface\\AddOns\\aegerUI\\Media\\"
 
 LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("aegerUI", {
 	type = "launcher",
@@ -8,35 +9,35 @@ LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("aegerUI", {
 			if topmenu:IsVisible() then
 				topmenu:Hide()
 				topmenuborder:Hide()
+				BazookaBar_1:Hide()
+				TopmenuShow = nil
+				PlaySoundFile(MEDIAPATH .. "Sound\\click.mp3")
 			else
 				topmenu:Show()
 				topmenuborder:Show()
-			
-			end
+				BazookaBar_1:Show()
+				TopmenuShow = 1
+				PlaySoundFile(MEDIAPATH .. "Sound\\click.mp3")
 			end
 		elseif msg == "RightButton" then
-			if Bbars = 1 then
-			if IsAddOnLoaded("Bartender4") then
-               Bartender4.db:SetProfile("aegerUI2bar")
-			   Bbars = 2
-			   ReloadUI()
-			   end
-			elseif
-			if Bbars = 2 then
-			if IsAddOnLoaded("Bartender4") then
-               Bartender4.db:SetProfile("aegerUI1bar")
+			if Bbars == 1 then
+			    Bartender4.db:SetProfile("aegerUI2bar")
+			    Bbars = 2
+			    ReloadUI()
+			else
+			if Bbars == 2 then
+			   Bartender4.db:SetProfile("aegerUI1bar")
 			   Bbars = 1
 			   ReloadUI()
+			   end
 			end
-			end
-			
 		end
 	end,
 	icon = "Interface\\AddOns\\aegerUI\\Media\\icon",
 	OnTooltipShow = function(tooltip)
 		if not tooltip or not tooltip.AddLine then return end
 		tooltip:AddLine("AegerUI")
-		tooltip:AddLine("|cffffff00" .. L_aegerUI["Click|r to toggle the top menu"])
-		tooltip:AddLine("|cffffff00" .. L_aegerUI["Right-click|r to toggle bottom bars"])
+		tooltip:AddLine("Click|r to toggle the top menu")
+		tooltip:AddLine("Right-click|r to toggle bottom bars")
 	end,
 })
